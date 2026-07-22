9956506534e31b07ecc40ad238fc416410e5ec2e
yad <yad@yad.com>
2026-07-22 21:58:49 +0200

    Trim whitespaces

--- ZygorGuidesViewerRM/Parser.lua
+++ ZygorGuidesViewerRM/Parser.lua
@@ -922,7 +922,7 @@ function me:ParseEntry(text)
 					if not plural then name=mob end
 
 					local nm,id = self:ParseID(name)
-					
+
 					tinsert(goal.mobs,{name=nm,id=id,pl=plural and true or false})
 				end
 			elseif cmd=="complete" then
@@ -994,7 +994,7 @@ function me:ParseEntry(text)
 				goal.action = goal.action or cmd
 				goal.recipe,goal.recipeid = self:ParseID(params)
 				if not goal.recipeid then return nil,"'learn': no recipe found",linecount,chunk end
-				
+
 			elseif cmd=="fpath" or cmd=="home" then
 				goal.action = goal.action or cmd
 				goal.param = params
@@ -1430,4 +1430,3 @@ function me:ParseEntry(text)
 	end
 	return guide
 end
-
